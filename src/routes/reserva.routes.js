29927bb35631b364c80262839860b6303e0e1ae8
Gugzz21 <gustavodinizz966@gmail.com>
2026-05-19 12:01:30 -0300

    mudança nomes de endpoint

--- src/routes/reserva.routes.js
+++ src/routes/reserva.routes.js
@@ -5,25 +5,25 @@ async function reservaRoutes(fastify, options) {
         throw new Error('[reservaRoutes] ReservasController instance must be provided in options.');
     }
 
-    fastify.post('/', controller.criar);
-    fastify.get('/', controller.listarAtivas);
-    fastify.get('/:id', controller.buscarPorId);
-    fastify.put('/:id', controller.atualizarTotal);
-    fastify.patch('/:id/status', controller.alterarStatus);
-    fastify.delete('/:id', controller.deletar);
-    fastify.get('/:id/historico', controller.buscarHistorico);
-    fastify.get('/usuario/:usuario_id', controller.buscarPorUsuario);
-    fastify.get('/livro/:livro_id/fila', controller.listarFilaLivro);
-    fastify.patch('/:id/renovar', controller.renovar);
-    fastify.get('/status/:status', controller.buscarPorStatus);
+    fastify.post('/criar', controller.criar);
+    fastify.get('/listar-ativas', controller.listarAtivas);
+    fastify.get('/listar/:id', controller.buscarPorId);
+    fastify.put('/atualizar/:id', controller.atualizarTotal);
+    fastify.patch('/atualizar-status/:id', controller.alterarStatus);
+    fastify.delete('/deletar/:id', controller.deletar);
+    fastify.get('/historico/:id', controller.buscarHistorico);
+    fastify.get('/usuario/listar/:usuario_id', controller.buscarPorUsuario);
+    fastify.get('/livro/listar-fila/:livro_id', controller.listarFilaLivro);
+    fastify.patch('/atualizar-data/:id', controller.renovar);
+    fastify.get('/status/listar/:status', controller.buscarPorStatus);
     fastify.get('/vencidas/listar', controller.listarVencidas);
     fastify.patch('/lote/cancelar-vencidas', controller.cancelarVencidasLote);
-    fastify.delete('/usuario/:usuario_id/limpar', controller.limparPorUsuario);
+    fastify.delete('/usuario/limpar/:usuario_id', controller.limparPorUsuario);
     fastify.get('/recentes/feed', controller.feedRecentes);
     fastify.post('/validar-conflito', controller.validarConflito);
     fastify.get('/metricas/pendentes', controller.contarPendentes);
-    fastify.get('/livro/:livro_id/contagem', controller.contarFilaLivro);
-    fastify.patch('/:id/notificar', controller.registrarNotificacao);
+    fastify.get('/livro/contagem/:livro_id', controller.contarFilaLivro);
+    fastify.patch('/registrar-notificacao/:id', controller.registrarNotificacao);
     fastify.get('/historico/geral', controller.feedHistoricoGeral);
 }
 
